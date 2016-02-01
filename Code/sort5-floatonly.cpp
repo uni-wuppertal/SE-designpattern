973ffbd0ab68ac91110825e7eff8e85c4de8b9bb
Hans Fangohr <notknown@here>
2016-02-01 10:31:56 +0100

    Kommentare in der richtigen Sprache.

--- Code/sort5-floatonly.cpp
+++ Code/sort5-floatonly.cpp
@@ -1,24 +1,26 @@
+// Mögliche Implementierung für UML Beispiel 'FloatData'
+
 #include <iostream>
 
-// limite data to N=5 items for example
+// N = 5 für dieses einfache Beispiel
 #define N 5
 
 class FloatData {
 public:
-    // Constructor, expects int array with 5 elements
+    // Konstructor, erwarted float array mit 5 Elementen.
     FloatData(float *array) {
         for (int i = 0; i < N; i++) 
             data[i] = array[i];
     }
 
-    // swap data with index i and index j
+    // Vertausche Elemente mit Index i und Index j.
     void swap(int i, int j) {
         float tmp = data[i];
         data[i] = data[j];
         data[j] = tmp;
     }
 
-    // comparison
+    // vergleiche Elemente mit Index i und j.
     bool greater(int i, int j) {
         if (data[i] > data[j])
             return true;
@@ -26,7 +28,9 @@ public:
             return false;
     }
 
-    // trivial sorting algorithm
+    // (Trivialer) Sortieralgorithmus, der 
+    // keinen direkten Datenzugriff braucht, sondern
+    // Interfacemethodes swap() und greater() benutzt.
     void sort(void) {
         for (int i=0; i < N; i++)
             for (int j=i+1; j < N; j++) {
@@ -35,7 +39,8 @@ public:
             }
         }
 
-    // reporting mechanism to stdout
+    // Reportmechanismus, der aktuelle Elemente berichtet.
+    // Hier einfach durch Ausgabe an standard output.
     void report(void) {
         std::cout << "[" << data[0];;
         for (int i=1; i < N; i++) {
@@ -48,17 +53,18 @@ private:
     float data[N];
 };
 
+
+// Hauptprogramm, das die Funktionalität demonstriert.
 int main(void) {
 
     float input_float[N] = {0.5, 1, -1, 2.5, -20};
     FloatData f(input_float);
 
     f.report();
-    f.swap(1, 2);
+    f.swap(0, 1);
     f.report();
     f.sort();
     f.report();
-
 }
 
 
